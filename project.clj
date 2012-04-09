2591b1b229d1176567216a8d01d1af78e4e0726b
Dmitri Naumov <ru.vinzent@gmail.com>
2012-04-09 20:46:54 +0600

    Added marginalia to deps

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.match "0.2.0-alpha9"]]
   :dev-dependencies [[midje "1.3.2-SNAPSHOT"]
-                     [lein-midje "1.0.7"]])
+                     [lein-midje "1.0.7"]
+                     [lein-marginalia "0.7.0"]])
